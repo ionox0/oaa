e6d3db78d864c5e4a4f107ade344ad3f5245c273
Ivan Storck <ivanoats@gmail.com>
2014-03-20 15:40:46 -0700

    removing debugging statement left in

--- config/passport.js
+++ config/passport.js
@@ -40,7 +40,6 @@ module.exports = function(passport) {
     // asynchronous
     // User.findOne wont fire unless data is sent back
     process.nextTick(function() {
-      console.log('HERE in nextTick');
 
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
